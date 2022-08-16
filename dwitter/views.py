717f9947db176db6813e963f3774d8e39665a19b
Vlad <vlads1976@gmail.com>
2022-08-16 13:36:20 +0200

    final commit. all is work.

--- dwitter/views.py
+++ dwitter/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
-from .models import Profile
+from django.shortcuts import render, redirect
 
+from .forms import DweetForm
+from .models import Dweet, Profile
 
 
 def dashboard(request):
-    return render(request, "dwitter/dashboard.htm")
+    form = DweetForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            dweet = form.save(commit=False)
+            dweet.user = request.user
+            dweet.save()
+            return redirect("dwitter:dashboard")
+
+    followed_dweets = Dweet.objects.filter(
+        user__profile__in=request.user.profile.follows.all()
+    ).order_by("-created_at")
+
+    return render(
+        request,
+        "dwitter/dashboard.html",
+        {"form": form, "dweets": followed_dweets},
+    )
 
 
 def profile_list(request):
@@ -13,19 +30,14 @@ def profile_list(request):
 
 
 def profile(request, pk):
-    if not hasattr(request.user, 'profile'):
-        missing_profile = Profile(user=request.user)
-        missing_profile.save()
-
     profile = Profile.objects.get(pk=pk)
-    if request.method == 'POST':
+    if request.method == "POST":
         current_user_profile = request.user.profile
         data = request.POST
-        action = data.get('follow')
-        if action == 'follow':
+        action = data.get("follow")
+        if action == "follow":
             current_user_profile.follows.add(profile)
-        elif action == 'unfollow':
+        elif action == "unfollow":
             current_user_profile.follows.remove(profile)
         current_user_profile.save()
-
     return render(request, "dwitter/profile.html", {"profile": profile})
